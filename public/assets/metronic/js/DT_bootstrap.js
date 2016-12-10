3ea29917d6614b02c672cbb6e58d5531c1db2ac8
syrator <recording@163.com>
2016-12-11 02:32:09 +0800

    为DataTables添加首页和尾页

--- public/assets/metronic/js/DT_bootstrap.js
+++ public/assets/metronic/js/DT_bootstrap.js
@@ -23,8 +23,10 @@ $.fn.dataTableExt.oApi.fnPagingInfo = function ( oSettings )
 		"iLength":        oSettings._iDisplayLength,
 		"iTotal":         oSettings.fnRecordsTotal(),
 		"iFilteredTotal": oSettings.fnRecordsDisplay(),
-		"iPage":          Math.ceil( oSettings._iDisplayStart / oSettings._iDisplayLength ),
-		"iTotalPages":    Math.ceil( oSettings.fnRecordsDisplay() / oSettings._iDisplayLength )
+//		"iPage":          Math.ceil( oSettings._iDisplayStart / oSettings._iDisplayLength ),
+        "iPage":          oSettings._iDisplayLength === -1 ? 0 : Math.ceil( oSettings._iDisplayStart / oSettings._iDisplayLength ),
+//		"iTotalPages":    Math.ceil( oSettings.fnRecordsDisplay() / oSettings._iDisplayLength )
+		"iTotalPages":    oSettings._iDisplayLength === -1 ? 0 : Math.ceil( oSettings.fnRecordsDisplay() / oSettings._iDisplayLength )
 	};
 };
 
@@ -43,13 +45,17 @@ $.extend( $.fn.dataTableExt.oPagination, {
 
 			$(nPaging).addClass('pagination').append(
 				'<ul>'+
+                	'<li class="first disabled"><a href="#"><span class="hidden-480">'+oLang.sFirst+'</span></a></li>'+
 					'<li class="prev disabled"><a href="#">&larr; <span class="hidden-480">'+oLang.sPrevious+'</span></a></li>'+
 					'<li class="next disabled"><a href="#"><span class="hidden-480">'+oLang.sNext+'</span> &rarr; </a></li>'+
+					'<li class="last disabled"><a href="#"><span class="hidden-480">'+oLang.sLast+'</span></a></li>'+
 				'</ul>'
 			);
 			var els = $('a', nPaging);
-			$(els[0]).bind( 'click.DT', { action: "previous" }, fnClickHandler );
-			$(els[1]).bind( 'click.DT', { action: "next" }, fnClickHandler );
+            $(els[0]).bind( 'click.DT', { action: "first" }, fnClickHandler );
+			$(els[1]).bind( 'click.DT', { action: "previous" }, fnClickHandler );
+			$(els[2]).bind( 'click.DT', { action: "next" }, fnClickHandler );
+            $(els[3]).bind( 'click.DT', { action: "last" }, fnClickHandler );
 		},
 
 		"fnUpdate": function ( oSettings, fnDraw ) {
@@ -75,13 +81,15 @@ $.extend( $.fn.dataTableExt.oPagination, {
 
 			for ( i=0, iLen=an.length ; i<iLen ; i++ ) {
 				// Remove the middle elements
-				$('li:gt(0)', an[i]).filter(':not(:last)').remove();
+//				$('li:gt(0)', an[i]).filter(':not(:last)').remove();
+				$('li:gt(1)', an[i]).filter(':lt(-2)').remove();
 
 				// Add the new list items and their event handlers
 				for ( j=iStart ; j<=iEnd ; j++ ) {
 					sClass = (j==oPaging.iPage+1) ? 'class="active"' : '';
 					$('<li '+sClass+'><a href="#">'+j+'</a></li>')
-						.insertBefore( $('li:last', an[i])[0] )
+//						.insertBefore( $('li:last', an[i])[0] )
+						.insertBefore( $('li:eq(-2)', an[i])[0] )
 						.bind('click', function (e) {
 							e.preventDefault();
 							oSettings._iDisplayStart = (parseInt($('a', this).text(),10)-1) * oPaging.iLength;
@@ -91,15 +99,19 @@ $.extend( $.fn.dataTableExt.oPagination, {
 
 				// Add / remove disabled classes from the static elements
 				if ( oPaging.iPage === 0 ) {
-					$('li:first', an[i]).addClass('disabled');
+//					$('li:first', an[i]).addClass('disabled');
+					$('li:lt(2)', an[i]).addClass('disabled');
 				} else {
-					$('li:first', an[i]).removeClass('disabled');
+//					$('li:first', an[i]).removeClass('disabled');
+					$('li:lt(2)', an[i]).removeClass('disabled');
 				}
 
 				if ( oPaging.iPage === oPaging.iTotalPages-1 || oPaging.iTotalPages === 0 ) {
-					$('li:last', an[i]).addClass('disabled');
+//					$('li:last', an[i]).addClass('disabled');
+					$('li:gt(-3)', an[i]).addClass('disabled');
 				} else {
-					$('li:last', an[i]).removeClass('disabled');
+//					$('li:last', an[i]).removeClass('disabled');
+					$('li:gt(-3)', an[i]).removeClass('disabled');
 				}
 			}
 		}
--- public/assets/metronic/js/table-managed.js
+++ public/assets/metronic/js/table-managed.js
@@ -5,57 +5,6 @@ var TableManaged = function () {
                 return;
             }
 
-            $('#sample_1').dataTable({
-                "aoColumns": [
-                  { "bSortable": false },
-                  null,
-                  { "bSortable": false },
-                  null,
-                  { "bSortable": false },
-                  { "bSortable": false }
-                ],
-                "aLengthMenu": [
-                    [5, 15, 20, -1],
-                    [5, 15, 20, "全部"]
-                ],
-                "iDisplayLength": 5,
-                "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
-                "sPaginationType": "bootstrap",
-                "oLanguage": {
-                    "sLengthMenu": "_MENU_ 记录  每页",
-                    "oPaginate": {
-                        "sPrevious": "上一页",
-                        "sNext": "下一页"                       
-                    },
-                    "sSearch": "查找：",
-        			"sInfo": "显示第 _START_ 到 _END_ 条记录，共  _TOTAL_ 条记录",
-        			"sInfoEmpty": "显示第0到0条记录，共0条记录",
-        			"sInfoFiltered": "(由_MAX_项记录过滤)",
-                    "sZeroRecords": "没有符合条件的记录"
-                },
-                "aoColumnDefs": [{
-                        'bSortable': false,
-                        'aTargets': [0]
-                    }
-                ]
-            });
-
-            jQuery('#sample_1 .group-checkable').change(function () {
-                var set = jQuery(this).attr("data-set");
-                var checked = jQuery(this).is(":checked");
-                jQuery(set).each(function () {
-                    if (checked) {
-                        $(this).attr("checked", true);
-                    } else {
-                        $(this).attr("checked", false);
-                    }
-                });
-                jQuery.uniform.update(set);
-            });
-
-            jQuery('#sample_1_wrapper .dataTables_filter input').addClass("m-wrap medium");
-            jQuery('#sample_1_wrapper .dataTables_length select').addClass("m-wrap small");
-
             $('#sample_2').dataTable({
                 "aLengthMenu": [
                     [5, 15, 20, -1],
@@ -68,7 +17,9 @@ var TableManaged = function () {
                     "sLengthMenu": "_MENU_ 每页",
                     "oPaginate": {
                         "sPrevious": "上一页",
-                        "sNext": "下一页"
+                        "sNext": "下一页",
+                        "sFirst": "首页",
+        				"sLast": "尾页",
                     },
                     "sSearch": "查找：",
         			"sInfo": "显示第 _START_ 到 _END_ 条记录，共  _TOTAL_ 条记录",
@@ -99,50 +50,6 @@ var TableManaged = function () {
             jQuery('#sample_2_wrapper .dataTables_filter input').addClass("m-wrap small");
             jQuery('#sample_2_wrapper .dataTables_length select').addClass("m-wrap small");
             jQuery('#sample_2_wrapper .dataTables_length select').select2();
-
-            $('#sample_3').dataTable({
-                "aLengthMenu": [
-                    [5, 15, 20, -1],
-                    [5, 15, 20, "全部"]
-                ],
-                "iDisplayLength": 5,
-                "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
-                "sPaginationType": "bootstrap",
-                "oLanguage": {
-                    "sLengthMenu": "_MENU_ 每页",
-                    "oPaginate": {
-                        "sPrevious": "上一页",
-                        "sNext": "下一页"
-                    },
-                    "sSearch": "查找：",
-        			"sInfo": "显示第 _START_ 到 _END_ 条记录，共  _TOTAL_ 条记录",
-        			"sInfoEmpty": "显示第0到0条记录，共0条记录",
-        			"sInfoFiltered": "(由_MAX_项记录过滤)",
-                    "sZeroRecords": "没有符合条件的记录"
-                },
-                "aoColumnDefs": [{
-                        'bSortable': false,
-                        'aTargets': [0]
-                    }
-                ]
-            });
-
-            jQuery('#sample_3 .group-checkable').change(function () {
-                var set = jQuery(this).attr("data-set");
-                var checked = jQuery(this).is(":checked");
-                jQuery(set).each(function () {
-                    if (checked) {
-                        $(this).attr("checked", true);
-                    } else {
-                        $(this).attr("checked", false);
-                    }
-                });
-                jQuery.uniform.update(set);
-            });
-
-            jQuery('#sample_3_wrapper .dataTables_filter input').addClass("m-wrap small");
-            jQuery('#sample_3_wrapper .dataTables_length select').addClass("m-wrap small");
-            jQuery('#sample_3_wrapper .dataTables_length select').select2();
         }
     };
 }();
