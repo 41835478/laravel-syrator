a671120a5e05adb82684917bdeeac166e89d0a16
syrator <recording@163.com>
2016-12-11 03:57:28 +0800

    模板管理-界面调整

--- resources/views/admin/back/system/theme/index.blade.php
+++ resources/views/admin/back/system/theme/index.blade.php
@@ -1,126 +1,124 @@
-@extends('admin.layout._back') 
+@extends('_layout._common')
 
-@section('content-header') 
-
+@section('head_css')
 @parent
-<h1>
-	系统管理<small>模板</small>
-</h1>
-<ol class="breadcrumb">
-	<li><a href="{{ site_url('home', 'admin') }}"><i class="fa fa-home"></i> 主页</a></li>
-	<li class="active">系统管理 - 模板</li>
-</ol>
-@stop 
+<link rel="stylesheet" type="text/css" href="{{ _asset('assets/metronic/css/select2_metro.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ _asset('assets/metronic/css/DT_bootstrap.css') }}" />
+@stop
 
-@section('content') 
+@section('body_attr') class="page-header-fixed" @stop
 
-@if(session()->has('message'))
-<div class="alert alert-success alert-dismissable">
-	<button type="button" class="close" data-dismiss="alert"
-		aria-hidden="true">×</button>
-	<h4>
-		<i class="icon fa fa-check"></i> 提示！
-	</h4>
-	{{ session('message') }}
-</div>
-@endif
+@section('content-header')
+@parent
+@include('admin._widgets._main-header')
+@stop
 
-<a href="{{ _route('admin:system.theme.create') }}" class="btn btn-primary margin-bottom">新增模板</a>
+@section('content-footer')
+@parent
+@include('admin._widgets._main-footer')
+@stop
 
-<div class="box box-primary">
-	<div class="box-header with-border">
-		<h3 class="box-title">模板列表</h3>
-	</div>	
-	<div class="box-body">
-		<table id="themes" class="table table-bordered table-striped">
-			<thead>
-                <tr>
-					<th>编号</th>
-					<th>编码</th>
-					<th>名称</th>
-					<th>作者</th>
-					<th>版本</th>
-					<th>是否使用</th>
-					<th>创建日期</th>
-					<th>更新日期</th>
-					<th>操作</th>
-                </tr>
-            </thead>
-			<tbody>                    
-				@foreach($themes as $index => $per) 
-				<tr>
-					<td class="text-muted">{{ $per->id }}</td>
-					<td class="text-green">{{ $per->code }}</td>
-					<td class="text-red">{{ $per->name }}</td>
-					<td class="text-red">{{ $per->author }}</td>
-					<td class="text-red">{{ $per->version }}</td>
-					@if($per->is_current === '1')
-					<td class="text-red"><i class="fa fa-check"><span style="display:none">是</span></td>
-					@else
-					<td class="text-red"><i class="fa fa-close"><span style="display:none">否</span></td>
-					@endif
-					<td>{{ $per->created_at }}</td>
-					<td>{{ $per->updated_at }}</td>
-					<td>
-						<a href="{{ _route('admin:system.theme.edit', $per->id) }}"><i class="fa fa-fw fa-pencil" title="修改"></i></a>
-						<a href="{{ _route('admin:system.theme.show', $per->id) }}" class="layer_open" data-title="查看" data-width="400"><i class="fa fa-fw fa-link" title="查看"></i></a>
-						<a href="javascript:void(0);" delid="{{ $per->id }}" class="remove" ><i class="fa fa-fw fa-remove" title="删除"></i></a>
-					</td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-    </div>
+@section('content')
+<div class="page-container row-fluid">
+	@include('admin._widgets._main-sidebar')
+	<div class="page-content">
+		<div id="portlet-config" class="modal hide">
+			<div class="modal-header">
+				<button data-dismiss="modal" class="close" type="button"></button>
+				<h3>Widget Settings</h3>
+			</div>
+			<div class="modal-body">
+				Widget settings form goes here
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span12">
+					<h3 class="page-title">模板管理  <small> 系统前台显示模板管理</small></h3>
+					<ul class="breadcrumb">
+						<li>
+							<i class="icon-home"></i>
+							<a href="{{ site_url('home', 'admin') }}">首页</a> 
+							<i class="icon-angle-right"></i>
+						</li>
+						<li><a href="#">系统模板</a></li>
+						<li class="pull-right no-text-shadow">
+							<div id="dashboard-report-range" class="dashboard-date-range tooltips no-tooltip-on-touch-device responsive" data-tablet="" data-desktop="tooltips" data-placement="top" data-original-title="Change dashboard date range">
+								<i class="icon-calendar"></i>
+								<span></span>
+								<i class="icon-angle-down"></i>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="row-fluid">
+				<div class="span6 responsive" data-tablet="span12 fix-offset" data-desktop="span6">
+					<div class="portlet box grey">
+						<div class="portlet-title">
+							<div class="caption">模板列表</div>
+						</div>
+						<div class="portlet-body">
+							<table class="table table-striped table-bordered table-hover" id="sample_2">
+								<thead>
+									<tr>
+										<th style="width:8px;text-align:center;"><input type="checkbox" class="group-checkable" data-set="#sample_2 .checkboxes" /></th>			
+                                        <th style="width:80px;text-align:center;">编码</th>
+										<th style="width:80px;text-align:center;">名称</th>
+                                        <th class="hidden-480" style="width:80px;text-align:center;">作者</th>
+                                        <th class="hidden-480" style="width:80px;text-align:center;">版本</th>
+                                        <th class="hidden-480" style="width:75px;text-align:center;">是否使用</th>
+                                        <th style="width:125px;text-align:center;">创建日期</th>
+                                        <th style="width:125px;text-align:center;">更新日期</th>
+                                        <th class="hidden-480" style="width:120px;text-align:center;">操作</th>
+									</tr>
+								</thead>
+								<tbody>
+                                    @foreach ($themes as $per)
+                                    <tr class="odd gradeX">
+										<td><input type="checkbox" class="checkboxes" value="{{ $per->id }}" /></td>
+                                        <td class="text-red">{{ $per->code }}</td>
+                                        <td class="text-green">{{ $per->name }}</td>
+                                        <td class="text-yellow">{{ $per->author }}</td>
+                                        <td>{{ $per->version }}</td>
+                    					@if($per->is_current === '1')
+                    					<td class="text-red" style="text-align:center;"><i class="icon-ok"></i></td>
+                    					@else
+                    					<td class="text-red" style="text-align:center;"><i class="icon-remove"></i></td>
+                    					@endif
+                    					<td style="text-align:center;">{{ $per->created_at }}</td>
+                    					<td style="text-align:center;">{{ $per->updated_at }}</td>
+                                        <td style="text-align:center;">
+                                        	<a class="edit" href="javascript:;"><i class="icon-eye-open"></i>Edit</a>
+                                        	<a class="edit" href="javascript:;"><i class="icon-eye-open"></i>Edit</a>
+                                        	<a class="edit" href="javascript:;"><i class="icon-eye-open"></i>Edit</a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @stop
 
 @section('extraPlugin')
-  <!--引入layer插件-->
-  <script src="{{ _asset(ref('layer.js')) }}"></script>
-  <script src="{{ _asset(ref('jquery.dataTables.min.js')) }}"></script>
-  <script src="{{ _asset(ref('dataTables.bootstrap.min.js')) }}"></script>
-  <link href="{{ _asset(ref('dataTables.bootstrap.css')) }}" rel="stylesheet" type="text/css" />
+@parent
+<script type="text/javascript" src="{{ _asset('assets/metronic/js/select2.min.js') }}"></script>
+<script type="text/javascript" src="{{ _asset('assets/metronic/js/jquery.dataTables.js') }}"></script>
+<script type="text/javascript" src="{{ _asset('assets/metronic/js/DT_bootstrap.js') }}"></script>
+<script type="text/javascript" src="{{ _asset('assets/metronic/js/table-managed.js') }}"></script>
 @stop
 
 @section('filledScript')
-
-    $("#themes").DataTable({
-    	"language": {            
-    		"url": "{{ _asset('lib/datatables/Chinese.json') }}"
-		}
-	});
-
-	$(document).on("click","a.layer_open",function(evt) {
-    	evt.preventDefault();
-        var that = this;
-        var src = $(this).attr("href");
-        var title = $(this).data('title');
-        layer.tips('这是你当前查看的权限', that);
-        layer.open({
-        	type: 2,
-            title: title,
-            shadeClose: false,
-            shade: 0,
-            area: ['600px', '360px'],
-            content: src //iframe的url
-        });
-    });
-    
-    $(document).on("click","a.remove",function(evt) {
-        var delId = $(this).attr("delId");
-    	if(confirm("删除后数据将无法恢复，您确定要删除?")){
-            $.post("{{ URL('admin/system/theme/remove') }}", {
-            	 _token:$('meta[name="_token"]').attr('content'),
-                 delId:delId,
-            }, function(data){
-                 if(data.code == 200){
-                     alert(data.message);
-                     location.reload();
-                 } else {
-                     alert(data.message);
-                 }
-            },"json");
-      	}
-    });
-
+<script>
+jQuery(document).ready(function() {    
+    App.init();
+    TableManaged.init();
+});
+</script>
 @stop
