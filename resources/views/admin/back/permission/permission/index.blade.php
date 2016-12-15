95bf688ca5914d0d379955952c3dfb20cd74d5aa
syrator <recording@163.com>
2016-12-15 14:40:02 +0800

    权限管理首页

--- resources/views/admin/back/permission/permission/index.blade.php
+++ resources/views/admin/back/permission/permission/index.blade.php
@@ -1,110 +1,177 @@
-@extends('admin.layout._back') 
+@extends('_layout._common')
 
-@section('content-header') 
-
+@section('head_css')
 @parent
-<h1>
-	权限管理<small>权限</small>
-</h1>
-<ol class="breadcrumb">
-	<li><a href="{{ site_url('home', 'admin') }}"><i class="fa fa-home"></i> 主页</a></li>
-	<li class="active">权限管理 - 权限</li>
-</ol>
-@stop 
+<link rel="stylesheet" type="text/css" href="{{ _asset('assets/metronic/css/select2_metro.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ _asset('assets/metronic/css/DT_bootstrap.css') }}" />
+@stop
 
-@section('content') 
+@section('body_attr') class="page-header-fixed" @stop
 
-@if(session()->has('message'))
-<div class="alert alert-success alert-dismissable">
-	<button type="button" class="close" data-dismiss="alert"
-		aria-hidden="true">×</button>
-	<h4>
-		<i class="icon fa fa-check"></i> 提示！
-	</h4>
-	{{ session('message') }}
-</div>
-@endif
+@section('content-header')
+@parent
+@include('admin._widgets._main-header')
+@stop
 
-<a href="{{ _route('admin:permission.permission.create') }}" class="btn btn-primary margin-bottom">新增权限</a>
+@section('content-footer')
+@parent
+@include('admin._widgets._main-footer')
+@stop
 
-<div class="box box-primary">
-	<div class="box-header with-border">
-		<h3 class="box-title">权限列表</h3>
-		<div class="box-tips clearfix">
-			<p>权限影响系统安全与稳定，错误或不合理的修改可能会影响系统业务与逻辑，请谨慎操作。</p>
+@section('content')
+<div class="page-container row-fluid">
+	@include('admin._widgets._main-sidebar')
+	<div class="page-content">
+		<div id="portlet-config" class="modal hide">
+			<div class="modal-header">
+				<button data-dismiss="modal" class="close" type="button"></button>
+				<h3>Widget Settings</h3>
+			</div>
+			<div class="modal-body">
+				Widget settings form goes here
+			</div>
 		</div>
-	</div>	
-	<div class="box-body">
-		<table id="permissions" class="table table-bordered table-striped">
-			<thead>
-                <tr>
-					<th>编号</th>
-					<th>权限名</th>
-					<th>权限展示名</th>
-					<th>创建日期</th>
-					<th>更新日期</th>
-					<th>操作</th>
-                </tr>
-            </thead>
-			<tbody>                    
-				@foreach($permissions as $index => $per) 
-				<tr>
-					<td class="text-muted">{{ $per->id }}</td>
-					<td class="text-green">{{ $per->name }}</td>
-					<td class="text-red">{{ $per->display_name }}</td>
-					<td>{{ $per->created_at }}</td>
-					<td>{{ $per->updated_at }}</td>
-					<td>
-						<a href="{{ _route('admin:permission.permission.edit', $per->id) }}"><i class="fa fa-fw fa-pencil" title="修改"></i></a>
-						<a href="{{ _route('admin:permission.permission.show', $per->id) }}" class="layer_open" data-title="查看" data-width="400"><i class="fa fa-fw fa-link" title="查看"></i></a>
-						<a href="javascript:void(0);" delid="{{ $per->id }}" class="remove" ><i class="fa fa-fw fa-remove" title="删除"></i></a>
-					</td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-    </div>
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span12">
+					<h3 class="page-title">权限管理  <small> 系统权限管理</small></h3>
+					<ul class="breadcrumb">
+						<li>
+							<i class="icon-home"></i>
+							<a href="{{ site_url('home', 'admin') }}">首页</a> 
+							<i class="icon-angle-right"></i>
+						</li>
+						<li><a href="#">权限管理</a></li>
+						<li class="pull-right no-text-shadow">
+							<div id="dashboard-report-range" class="dashboard-date-range tooltips no-tooltip-on-touch-device responsive" 
+								data-tablet="" data-desktop="tooltips" data-placement="top" data-original-title="Change dashboard date range">
+								<i class="icon-calendar"></i>
+								<span></span>
+								<i class="icon-angle-down"></i>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="row-fluid">
+				<div class="span6 responsive" data-tablet="span12 fix-offset" data-desktop="span6">
+					<div class="portlet box grey">
+                        @if(session()->has('fail'))
+                        <div class="alert alert-warning alert-dismissable">
+                        	<button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
+                        	<h4>
+                        		<i class="icon icon fa fa-warning"></i> 提示！
+                        	</h4>
+                        	{{ session('fail') }}
+                        </div>
+                        @endif 
+                        
+                        @if($errors->any())
+                        <div class="alert alert-danger alert-dismissable">
+                        	<button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
+                        	<h4>
+                        		<i class="icon fa fa-ban"></i> 警告！
+                        	</h4>
+                        	<ul>
+                        		@foreach ($errors->all() as $error)
+                        		<li>{{ $error }}</li> 
+                        		@endforeach
+                        	</ul>
+                        </div>
+                        @endif
+						<div class="portlet-title">
+							<div class="caption">权限列表</div>
+							<div class="actions">
+								<div class="btn-group">
+									<a href="{{ _route('admin:permission.permission.create') }}" class="btn"><i class="icon-pencil"></i> 新增</a>
+								</div>
+							</div>	
+						</div>
+						<div class="portlet-body">
+							<table class="table table-striped table-bordered table-hover" id="syrator_table_permission">							
+								<thead>
+									<tr>
+										<th style="width:8px;text-align:center;">
+											<input type="checkbox" class="group-checkable" data-set="#syrator_table_permission .checkboxes" />
+										</th>			
+										<th style="">权限名</th>
+										<th style="">权限展示名</th>
+										<th style="">创建日期</th>
+										<th style="">更新日期</th>
+                                        <th class="hidden-480" style="text-align:center;">操作</th>
+									</tr>
+								</thead>
+								<tbody>
+                                    @foreach ($permissions as $per)
+                                    <tr class="odd gradeX">
+										<td style="width:8px;text-align:center;">
+											<input type="checkbox" class="checkboxes" value="{{ $per->id }}" />
+										</td>
+                                        <td class="text-green">{{ $per->name }}</td>
+                                        <td class="text-green">{{ $per->display_name }}</td>
+                                        <td>{{ $per->created_at }}</td>
+                                        <td>{{ $per->updated_at }}</td>
+                    					<td style="text-align: center;">                                        	
+                                        	<a data-title="{{ $per->display_name }}" href="{{ _route('admin:permission.permission.show', $per->id) }}" role="button" class="layer_open btn btn-danger" style="background: none;padding:3px;">
+                                        		<i class="icon-eye-open"></i>
+                                        	</a>
+                                        	<a href="{{ _route('admin:permission.permission.edit', $per->id) }}" role="button" class="btn btn-danger" style="background: none;padding:3px;">
+                                        		<i class="icon-edit"></i>
+                                        	</a>
+                                        	<a item-id="{{ $per->id }}" href="javascript:void(0);" role="button" class="remove btn btn-danger" style="background: none;padding:3px;">
+                                        		<i class="icon-trash"></i>
+                                        	</a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @stop
 
 @section('extraPlugin')
-  <!--引入layer插件-->
-  <script src="{{ _asset(ref('layer.js')) }}"></script>
-  <script src="{{ _asset(ref('jquery.dataTables.min.js')) }}"></script>
-  <script src="{{ _asset(ref('dataTables.bootstrap.min.js')) }}"></script>
-  <link href="{{ _asset(ref('dataTables.bootstrap.css')) }}" rel="stylesheet" type="text/css" />
+@parent
+<script type="text/javascript" src="{{ _asset('assets/metronic/js/select2.min.js') }}"></script>
+<script type="text/javascript" src="{{ _asset('assets/metronic/js/jquery.dataTables.js') }}"></script>
+<script type="text/javascript" src="{{ _asset('assets/metronic/js/jquery.dataTables.columnFilter.js') }}"></script>
+<script type="text/javascript" src="{{ _asset('assets/metronic/js/DT_bootstrap.js') }}"></script>
+<script type="text/javascript" src="{{ _asset('assets/js/table-permission-permission.js') }}"></script> 
+<script type="text/javascript" src="{{ _asset(ref('layer.js')) }}"></script>
 @stop
 
 @section('filledScript')
+<script>
+jQuery(document).ready(function() {    
+    App.init();
+    TablePermission.init();
 
-    $("#permissions").DataTable({
-    	"language": {            
-    		"url": "{{ _asset('lib/datatables/Chinese.json') }}"
-		}
-	});
-
-	$(document).on("click","a.layer_open",function(evt) {
-    	evt.preventDefault();
+    $(document).on("click","a.layer_open",function(evt) {
+        evt.preventDefault();
         var that = this;
         var src = $(this).attr("href");
         var title = $(this).data('title');
-        layer.tips('这是你当前查看的权限', that);
         layer.open({
-        	type: 2,
+            type: 2,
             title: title,
             shadeClose: false,
             shade: 0,
-            area: ['600px', '360px'],
-            content: src //iframe的url
+            area: ['480px', '283px'],
+            content: src
         });
     });
-    
+
     $(document).on("click","a.remove",function(evt) {
-        var delId = $(this).attr("delId");
+        var itemId = $(this).attr("item-id");
     	if(confirm("删除后数据将无法恢复，您确定要删除?")){
             $.post("{{ URL('admin/permission/permission/remove') }}", {
             	 _token:$('meta[name="_token"]').attr('content'),
-                 delId:delId,
+                 delId:itemId,
             }, function(data){
                  if(data.code == 200){
                      alert(data.message);
@@ -115,5 +182,6 @@
             },"json");
       	}
     });
-
+});
+</script>
 @stop
