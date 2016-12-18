a569014608839461a88913256d8e62b65a9c6777
syrator <recording@163.com>
2016-12-19 01:40:02 +0800

    修改目录名称的大小写

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,7 +8,7 @@ require __DIR__.'/Routes/routes-api.php';           // api
 require __DIR__.'/Routes/routes-documents.php';     // 文档
 
 // 应用路由
-require __DIR__.'/Routes/cms/routes-cms.php';       // cms应用
+require __DIR__.'/Routes/CMS/routes-cms.php';       // cms应用
 
 // 管理后台站点路由群组
 Route::group(['prefix' => config('site.route.prefix.admin', 'admin'), 'namespace' => 'Admin', 'middleware' => ['block:admin', 'web']], function () {
