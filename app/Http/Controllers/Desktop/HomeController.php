64771c347fdb125ec6d0f648620442c56e83e782
syrator <recording@163.com>
2017-01-04 22:10:32 +0800

    syrator <recording@163.com>

--- app/Http/Controllers/Desktop/HomeController.php
+++ app/Http/Controllers/Desktop/HomeController.php
@@ -4,10 +4,29 @@ namespace App\Http\Controllers\Desktop;
 
 use App\Jobs\ChangeLocale;
 
+use EasyWeChat\Foundation\Application;
+
 class HomeController extends FrontController
 {
     public function getIndex()
     {
+        $options = [
+            'debug'     => true,
+            'app_id'    => 'wx3cf0f39249eb0e60',
+            'secret'    => 'f1c242f4f28f735d4687abb469072a29',
+            'token'     => 'laravel-syrator-easywechat',
+            'log' => [
+                'level' => 'debug',
+                'file'  => '/tmp/easywechat.log',
+            ],
+            // ...
+        ];
+        
+        $app = new Application($options);
+        
+        $server = $app->server;
+        $user = $app->user;
+        
         return $this->view('index');
     }
 
