e1a3285eb792052ce3371328746e157ad43133d4
syrator <recording@163.com>
2017-01-05 10:55:31 +0800

    微信同步登陆

--- app/Http/Controllers/Desktop/HomeController.php
+++ app/Http/Controllers/Desktop/HomeController.php
@@ -3,30 +3,12 @@
 namespace App\Http\Controllers\Desktop;
 
 use App\Jobs\ChangeLocale;
-
-use EasyWeChat\Foundation\Application;
+use Illuminate\Http\Request;
 
 class HomeController extends FrontController
 {
-    public function getIndex()
+    public function getIndex(Request $request)
     {
-        $options = [
-            'debug'     => true,
-            'app_id'    => 'wx13b647618165d0c6',
-            'secret'    => '7385e8edde0b99bb5684b70790e0e688',
-            'token'     => 'laravel-syrator-easywechat',
-            'log' => [
-                'level' => 'debug',
-                'file'  => '/tmp/easywechat.log',
-            ],
-            // ...
-        ];
-        
-        $app = new Application($options);
-        
-        $server = $app->server;
-        $user = $app->user;
-        
         return $this->view('index');
     }
 
