5285af3e976a4a90fe98ff625f4c103bf4de7d98
syrator <recording@163.com>
2017-01-05 11:01:36 +0800

    微信登陆逻辑整理

--- app/Http/Controllers/Mobile/HomeController.php
+++ app/Http/Controllers/Mobile/HomeController.php
@@ -13,25 +13,6 @@ class HomeController extends MobileController
             return redirect()->to(site_path('member/login', 'mobile'));
         }
         
-        $member = $request->session()->get('member');
-        if ($member==null || empty($member)) {
-            $options = [
-                'debug'     => true,
-                'app_id'    => 'wx13b647618165d0c6',
-                'secret'    => '7385e8edde0b99bb5684b70790e0e688',
-                'token'     => 'laravel-syrator',
-                'oauth'     => [
-                    'scopes'   => ['snsapi_userinfo'],
-                    'callback' => '/mobile/member/login',
-                ],
-            ];
-        
-            $app = new Application($options);
-            $oauth = $app->oauth;
-        
-            return $oauth->redirect();
-        }
-        
         return $this->view('index');
     }
 }
