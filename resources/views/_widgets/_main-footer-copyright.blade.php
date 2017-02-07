419c14bdb4d3b6506921ed99d1eb06872f052794
syrator <recording@163.com>
2017-02-07 17:51:56 +0800

    syrator <recording@163.com>

--- resources/views/_widgets/_main-footer-copyright.blade.php
+++ resources/views/_widgets/_main-footer-copyright.blade.php
@@ -1,5 +1,7 @@
 {{-- widget.main-footer-copyright --}}
-<strong>Copyright &copy; 2015-{{ date('Y') }} 
-	<a href="http://www.syrator.com/">{{ trans('syrator.framework_name')}}</a>
-</strong> 
-(<code>{{ syrator_version() }}</code>)
+<div class="container">
+	<div class="pull-right hidden-xs">
+		<b>版本:</b> {{ syrator_version() }}
+	</div>
+	<strong>Copyright &copy; 2011-{{ date('Y') }} <a href="http://www.syrator.com/">{{ trans('syrator.framework_name')}}</a></strong>
+</div>
