789ffdd3930d1ef67993cb1195dfdc298f6d16de
syrator <recording@163.com>
2017-02-08 15:31:58 +0800

    syrator <recording@163.com>

--- modules/Wechat/Resources/views/index.blade.php
+++ modules/Wechat/Resources/views/index.blade.php
@@ -1,11 +1,52 @@
-@extends('wechat::layouts.master')
+@extends('_layout._common')
+
+@section('head_css')
+@parent
+@stop
+
+@section('body_attr') class="page-header-fixed" @stop
+
+@section('content-header')
+@parent
+@include('admin._widgets._main-header')
+@stop
+
+@section('content-footer')
+@parent
+@include('admin._widgets._main-footer')
+@stop
 
 @section('content')
-	
-	<h1>Hello World</h1>
-	
-	<p>
-		This view is loaded from module: {!! config('wechat.name') !!}
-	</p>
+<div class="page-container">
+	@include('admin._widgets._main-sidebar')
+	<div class="page-content">
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span12">
+					<h3 class="page-title">{{cache('website_title')}}后台管理控制台</h3>
+					<ul class="breadcrumb">
+						<li>
+							<i class="icon-home"></i>
+							<a href="#">首页</a> 
+							<i class="icon-angle-right"></i>
+						</li>
+						<li><a href="#">控制台</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>  
+	</div>
+</div>
+@stop
 
+@section('extraPlugin')
+@parent
+@stop
+
+@section('filledScript')
+<script>
+jQuery(document).ready(function() {    
+   App.init();
+});
+</script>
 @stop
