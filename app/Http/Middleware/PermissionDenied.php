ce79f9027ed4a355fb6f04e31086c6bc5c9a81c4
syrator <recording@163.com>
2017-05-20 16:50:09 +0800

    syrator <recording@163.com>

--- app/Http/Middleware/PermissionDenied.php
+++ app/Http/Middleware/PermissionDenied.php
@@ -20,7 +20,7 @@ class PermissionDenied
      */
     public function handle($request, Closure $next)
     {
-        return response()->view('admin.exceptions.deny', array(), 403);
+        return response()->view('admin.errors.403', array(), 403);
         return $next($request);
     }
 }
