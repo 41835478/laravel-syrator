b325dc80f47c49e7622c064058493d51b9e6a730
syrator <recording@163.com>
2017-05-22 14:24:58 +0800

    syrator <recording@163.com>

--- app/Http/Controllers/Admin/Mine/MineController.php
+++ app/Http/Controllers/Admin/Mine/MineController.php
@@ -3,66 +3,62 @@
 namespace App\Http\Controllers\Admin\Mine;
 
 use App\Http\Controllers\Admin\BackController;
+use Zizaco\Entrust\EntrustFacade as Entrust;
 
 use App\Http\Requests\MeRequest;
 use Illuminate\Http\Request;
 use App\Repositories\UserRepository;
 
-/**
- * 我的账户控制器
- *
- */
-class MeController extends BackController
+class MineController extends BackController
 {
     protected $user;
-
 
     public function __construct(UserRepository $user)
     {
         parent::__construct();
         $this->user = $user;
+        
+        if(!Entrust::can('admin.mine')) {
+            $this->middleware('deny');
+        }
     }
 
-    /**
-     * 个人资料页面
-     *
-     * @return Response
-     */
     public function getMeInforation()
     {
+        if(!Entrust::can('admin.mine.inforation')) {
+            return deny();
+        }
+        
         $me = auth()->user();
         return $this->view('mine.index', compact('me'));
     }
 
-    /**
-     * 提交修改个人资料
-     *
-     * @return Response
-     */
     public function putMeInforation(MeRequest $request)
     {        
+        if(!Entrust::can('admin.mine.inforation')) {
+            return deny();
+        }
+        
         $this->user->updateMe(auth()->user(), $request->all());
         return redirect()->back()->with('message', '成功更新个人资料！');
     }
     
-    /**
-     * 提交修改头像
-     *
-     * @return Response
-     */
     public function putMeAvatar(Request $request)
-    {        
+    {
+        if(!Entrust::can('admin.mine.avatar')) {
+            return deny();
+        }
+        
         $this->user->updateMeAvatar(auth()->user(), $request->all());
         return redirect()->back()->with('message', '成功更新个人头像！');
     }    
     
-    /**
-     * 提交新密码
-     *
-     * @return Response
-     */
     public function putMePassword(MeRequest $request)
-    {        
+    {  
+        if(!Entrust::can('admin.mine.password')) {
+            return deny();
+        }
+        
         $this->user->updateMe(auth()->user(), $request->all());
         return redirect()->back()->with('message', '成功更新密码！');
     }
