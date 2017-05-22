005e7ee4144cd342c918acdc3b26d3dbd6c78fbb
syrator <recording@163.com>
2017-05-22 14:55:59 +0800

    syrator <recording@163.com>

--- app/Http/Controllers/Admin/System/AppInfo/AppInfoController.php
+++ app/Http/Controllers/Admin/System/AppInfo/AppInfoController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers\Admin\System\AppInfo;
 
 use App\Http\Controllers\Admin\BackController;
+use Zizaco\Entrust\EntrustFacade as Entrust;
 
 class AppInfoController extends BackController
 {
+    public function __construct()
+    {
+        parent::__construct();
+    
+        if(!Entrust::can('admin.system.appinfo')) {
+            $this->middleware('deny');
+        }
+    }
+    
     public function index()
     {
+        if(!Entrust::can('admin.system.appinfo')) {
+            return deny();
+        }
+        
         return redirect()->to(site_path('system/appinfo/guide', 'admin'));
     }
 }
