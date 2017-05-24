e291619ff7e24c8fcdd35309ba444f085255289a
syrator <recording@163.com>
2017-05-24 14:43:10 +0800

    syrator <recording@163.com>

--- app/Http/Routes/routes-mobile.php
+++ app/Http/Routes/routes-mobile.php
@@ -5,4 +5,13 @@ Route::group(['prefix' => 'mobile', 'namespace' => 'Mobile', 'middleware' => ['b
     // 首页
     Route::get('/', 'HomeController@getIndex');
     Route::get('index', 'HomeController@getIndex');
+    
+    // 会员
+    Route::group(['prefix' => 'member', 'namespace' => 'Member'], function () {
+        Route::resource('login', 'MemberController@login');
+        Route::resource('loginfast', 'MemberController@loginFast');
+        Route::resource('logout', 'MemberController@logout');
+        Route::resource('register', 'MemberController@register');
+        Route::resource('resetpassword', 'MemberController@resetPassword');
+    });
 });
