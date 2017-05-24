c1f17c9e53540f9e090af8386292575519cae499
syrator <recording@163.com>
2017-05-24 14:10:34 +0800

    syrator <recording@163.com>

--- app/Http/Routes/routes-mobile.php
+++ app/Http/Routes/routes-mobile.php
@@ -2,5 +2,7 @@
 
 // mobile前台站点路由群组
 Route::group(['prefix' => 'mobile', 'namespace' => 'Mobile', 'middleware' => ['block:mobile', 'web']], function () {
-
+    // 首页
+    Route::get('/', 'HomeController@getIndex');
+    Route::get('index', 'HomeController@getIndex');
 });
