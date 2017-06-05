e2bf94c2bcac1f36f858f6d94cc3435068aed993
syrator <recording@163.com>
2017-06-05 16:35:48 +0800

    syrator <recording@163.com>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+use App\Loggers\SystemLogger;
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -27,7 +29,12 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->call(function () {
-            DB::table('system_logs')->delete();
-        })->everyMinute();
+            $log = [
+                'user_id' => 0,
+                'type'    => 'schedule',
+                'content' => 'schedule',
+            ];
+            SystemLogger::write($log);
+        })->everyFiveMinutes();
     }
 }
