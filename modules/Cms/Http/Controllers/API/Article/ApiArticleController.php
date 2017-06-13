fd1d88584c7e06570ea5671f09c6f2c9a6d3abf6
syrator <recording@163.com>
2017-06-13 17:52:50 +0800

    syrator <recording@163.com>

--- modules/Cms/Http/Controllers/API/Article/ApiArticleController.php
+++ modules/Cms/Http/Controllers/API/Article/ApiArticleController.php
@@ -1,67 +1,25 @@
 <?php
 
-namespace Modules\Cms\Http\Controllers\API\Forum;
+namespace Modules\Cms\Http\Controllers\API\Article;
 
 use Modules\Cms\Http\Controllers\API\ApiBaseController;
 
 use Illuminate\Http\Request;
 
-use Modules\Cms\Repositories\ForumTitleRepository;
+use Modules\Cms\Model\ArticleCatalogModel;
 
 class ApiArticleController extends ApiBaseController
 {
-    public function __construct(ForumTitleRepository $repository)
+    public function __construct()
     {
         parent::__construct();
-        $this->repository = $repository;
     }
     
     /**
      * @SWG\Post(
-     *   path="/forum/list",
-     *   summary="获取所有的论坛文章列表",
-     *   tags={"Forum:论坛模块"},
-     *   @SWG\Response(
-     *     response=200,
-     *     description="请求成功且业务逻辑正确"
-     *   ),
-     *   @SWG\Response(
-     *     response=299,
-     *     description="请求成功但业务逻辑上有错误(具体看返回数据中的code和description)"
-     *   ),
-     *   @SWG\Response(
-     *     response="500",
-     *     description="系统错误"
-     *   )
-     * )
-     */
-    public function getList(Request $request)
-    {
-        // 获取参数
-        $param = [
-            'sortby' => $request->input('sortby'),
-            'orderby' => $request->input('orderby'),
-        ];
-        
-        // 参数检测
-        if (empty(e($param['sortby']))) {
-            $param['sortby'] = "default";
-        }
-        
-        if (empty(e($param['orderby']))) {
-            $param['orderby'] = "desc";
-        }
-        
-        $listForumTitle= $this->repository->index();
-
-        return self::responseSuccess('成功',$listForumTitle);
-    }
-    
-    /**
-     * @SWG\Post(
-     *   path="/forum/getcatalogs",
+     *   path="/article/getcatalogs",
      *   summary="获取所有的论坛模块列表",
-     *   tags={"Forum"},
+     *   tags={"Article"},
      *   @SWG\Parameter(name="format_type", in="query", required=false, type="string", description="返回数据格式  tree:树形结构的json list:单纯的列表结构"),
      *   @SWG\Response(
      *     response=200,
@@ -88,9 +46,18 @@ class ApiArticleController extends ApiBaseController
         if (empty(e($param['format_type']))) {
             $param['format_type'] = 'tree';
         }
-    
-        $catalogs = $this->repository->getCatalogs($param);
-    
+        
+        if ($param['format_type'] == "tree") {        
+            $catalogs = ArticleCatalogModel::recCatalogs(0);    
+            return self::responseSuccess('成功',$catalogs);
+        }
+        
+        if ($param['format_type'] == "list") {
+            $catalogs = ArticleCatalogModel::all();  
+            return self::responseSuccess('成功',$catalogs);
+        }
+        
+        $catalogs = ArticleCatalogModel::recCatalogs(0);    
         return self::responseSuccess('成功',$catalogs);
     }
 }
