b42cfacba8e9fcf03ad21eb1e405c0813834a07c
Shu Li <2625874928@qq.com>
2017-06-19 15:45:04 +0800

    Shu Li <2625874928@qq.com>

--- modules/Cms/Resources/views/desktop/_widgets/_main-sidebar.blade.php
+++ modules/Cms/Resources/views/desktop/_widgets/_main-sidebar.blade.php
@@ -1,26 +1,24 @@
 <div id="sidebar_article_left" class="article_left">
     <div class="article_l_c">
-    	<h2 class="article_l_t">Recoding127的博客</h2>
-    	<h6 class="article_l_sign"></h6>
         <div class="item_wrap">
             <dl class="item_l">
-                <dt class="item">2</dt>
+                <dt class="item_value">2</dt>
                 <dd class="item_name">原创</dd>
             </dl>
             <dl class="item_l">
-                <dt class="item">0</dt>
+                <dt class="item_value">0</dt>
                 <dd class="item_name">转载</dd>
             </dl>
             <dl class="item_l">
-                <dt class="item">0</dt>
+                <dt class="item_value">0</dt>
                 <dd class="item_name">译文</dd>
             </dl>
             <dl class="item_l">
-                <dt class="item">0</dt>
+                <dt class="item_value">0</dt>
                 <dd class="item_name">评论</dd>
             </dl>
             <dl class="item_l">
-                <dt class="item">69</dt>
+                <dt class="item_value">69</dt>
                 <dd class="item_name">访问</dd>
             </dl>
         </div>
