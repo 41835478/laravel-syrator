e145ce6a75334a2a43eb0635960d6d1f5a3527d5
Shu Li <2625874928@qq.com>
2017-06-19 14:21:29 +0800

    Shu Li <2625874928@qq.com>

--- modules/Cms/Resources/views/desktop/_widgets/_main-sidebar.blade.php
+++ modules/Cms/Resources/views/desktop/_widgets/_main-sidebar.blade.php
@@ -1,7 +1,7 @@
-<div class="blog_left">
-    <div class="blog_l_c">
-    	<h2 class="blog_l_t">Recoding127的博客</h2>
-    	<h6 class="blog_l_sign"></h6>
+<div id="sidebar_article_left" class="article_left">
+    <div class="article_l_c">
+    	<h2 class="article_l_t">Recoding127的博客</h2>
+    	<h6 class="article_l_sign"></h6>
         <div class="item_wrap">
             <dl class="item_l">
                 <dt class="item">2</dt>
@@ -25,12 +25,12 @@
             </dl>
         </div>
     </div>
-    <div class="blog_copyright">
+    <div class="article_copyright">
         <p>
-        	<span>京 ICP 证 070598 号</span>
+        	<span>鄂ICP备15009849号-1</span>
         </p>
         <p>
-            Copyright &copy; 1999-2016,
+            Copyright &copy; 1999-2046,
             <br>
             CSDN.NET, All Rights Reserved
         </p>
