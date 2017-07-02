a2e33acdbbd2ef07fa14c27c94a7b2ff62c94697
Shu Li <2625874928@qq.com>
2017-07-02 22:32:41 +0800

    Shu Li <2625874928@qq.com>

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -11,23 +11,29 @@ class="page-header-fixed page-sidebar-closed-hide-logo page-content-white"
 @section('content')
 <div class="page-wrapper">
 	@include('admin._widgets._main-header')
-	@include('admin._widgets._main-sidebar')
-	<div class="page-content" style="min-height:993px !important">
-		<div class="container-fluid">
-			<div class="row-fluid">
-				<div class="span12">
-					<h3 class="page-title">{{cache('website_title')}}后台管理控制台</h3>
-					<ul class="breadcrumb">
-						<li>
-							<i class="icon-home"></i>
-							<a href="#">首页</a> 
-							<i class="icon-angle-right"></i>
-						</li>
-						<li><a href="#">控制台</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>  
+	<div class="page-container">
+		@include('admin._widgets._main-sidebar')
+		<div class="page-content-wrapper">
+			<div class="page-content">
+				<div class="page-bar">
+                    <ul class="page-breadcrumb">
+                        <li>
+                            <a href="#">首页</a>
+                            <i class="fa fa-circle"></i>
+                        </li>
+                        <li>
+                            <span>控制台</span>
+                        </li>
+                    </ul>
+                </div>                
+                <h1 class="page-title"> {{cache('website_title')}}
+                    <small>后台管理控制台</small>
+                </h1>
+                <div class="note note-info">
+                    <p> {{cache('website_title')}}后台管理系统 </p>
+                </div>
+    		</div>
+		</div>
 	</div>	
 	@include('admin._widgets._main-footer')
 </div>
