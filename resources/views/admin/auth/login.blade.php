07fd7745173b460306ab6205ff97eca182c2bad4
Shu Li <2625874928@qq.com>
2017-07-03 17:39:14 +0800

    Shu Li <2625874928@qq.com>

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,7 +1,12 @@
 @extends('_layout._login')
 
-@section('head_css')
+@section('head_style')
 @parent
+<style>
+.login {
+    background-color: #3d3d3d!important;
+}
+</style>
 @stop
 
 @section('body_attr') class="login" @stop
@@ -13,15 +18,12 @@
 
 @section('content')
 <div class="logo">
-	<img src="" alt="" /> 
-</div>
-<div class="logo">
     <a href="{{ site_url('', '') }}">
         <img src="{{ _asset('assets/metronic/pages/img/logo-big.png') }}" alt="" /> 
     </a>
 </div>
 <div class="content">
-	<form class="form-vertical login-form" method="post" action="{{ site_url('auth/login', 'admin') }}">
+	<form class="login-form" method="post" action="{{ site_url('auth/login', 'admin') }}">
 		{{ csrf_field() }}
 		<h3 class="form-title" style="text-align: center">登录后台管理系统</h3>
 		@if (count($errors) > 0)
@@ -31,36 +33,31 @@
             <p>{!! $errors->first('attempt') !!}</p>
         </div>
         @endif
-		<div class="alert alert-error hide">
+		<div class="alert alert-danger display-hide">
 			<button class="close" data-dismiss="alert"></button>
 			<span>请输入用户名和密码</span>
 		</div>
-		<div class="control-group">
+		<div class="form-group">
 			<label class="control-label visible-ie8 visible-ie9">用户名</label>
-			<div class="controls">
-				<div class="input-icon left">
-					<i class="icon-user"></i>
-					<input class="m-wrap placeholder-no-fix" type="text" placeholder="用户名" name="username"/>
-				</div>
-			</div>
+    		<div class="input-icon">
+    			<i class="fa fa-user"></i>
+                <input class="form-control placeholder-no-fix" type="text" placeholder="用户名" name="username" />
+    		</div>
 		</div>
-		<div class="control-group">
+		<div class="form-group">
 			<label class="control-label visible-ie8 visible-ie9">密码</label>
-			<div class="controls">
-				<div class="input-icon left">
-					<i class="icon-lock"></i>
-					<input class="m-wrap placeholder-no-fix" type="password" placeholder="密码" name="password"/>
-				</div>
+			<div class="input-icon">
+				<i class="fa fa-lock"></i>
+				<input class="form-control placeholder-no-fix" type="password" placeholder="密码" name="password"/>
 			</div>
-		</div>
-		<div class="form-actions">
-			<label class="checkbox">
-			<input type="checkbox" name="remember" value="1"/> 记住我
-			</label>
-			<button type="submit" class="btn green pull-right">
-			登录 <i class="m-icon-swapright m-icon-white"></i>
-			</button>            
-		</div>
+		</div>		
+        <div class="form-actions">
+            <label class="rememberme mt-checkbox mt-checkbox-outline">
+                <input type="checkbox" name="remember" value="1" /> 记住我
+                <span></span>
+            </label>
+            <button type="submit" class="btn green pull-right"> 登录 </button>
+        </div>
 		<div class="forget-password">
 			<h4>忘记密码 ?</h4>
 			<p>
