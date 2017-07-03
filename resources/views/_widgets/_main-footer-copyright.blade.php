f68c6a5e9b24f365c17c4583116f8cab2918de30
Shu Li <2625874928@qq.com>
2017-07-03 16:41:24 +0800

    Shu Li <2625874928@qq.com>

--- resources/views/_widgets/_main-footer-copyright.blade.php
+++
@@ -1,7 +0,0 @@
-{{-- widget.main-footer-copyright --}}
-<div class="container">
-	<div class="pull-right hidden-xs">
-		<b>版本:</b> {{ syrator_version() }}
-	</div>
-	<strong>Copyright &copy; 2011-{{ date('Y') }} <a href="http://www.syrator.com/">{{ trans('syrator.framework_name')}}</a></strong>
-</div>
