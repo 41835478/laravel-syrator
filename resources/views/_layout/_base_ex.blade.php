6ada5c677a23c67bb1da8263da821c77bd925f23
Shu Li <2625874928@qq.com>
2017-07-03 15:09:16 +0800

    Shu Li <2625874928@qq.com>

---
+++ resources/views/_layout/_base_ex.blade.php
@@ -0,0 +1,75 @@
+@extends('_layout._base')
+
+@section('head_css')
+@parent
+	@section('head_css_mandatory')
+    @show
+    
+    @section('head_css_page_level_plugins')
+    @show
+    
+    @section('head_css_theme_global')
+    @show
+    
+    @section('head_css_page_level')
+    @show
+    
+    @section('head_css_theme_layout')
+    @show
+    
+	<link href="{{ _asset('assets/css/syrator.css') }}" rel="stylesheet" type="text/css"/>
+@stop
+
+@section('head_js')
+@parent
+    <!--[if lt IE 9]>
+    <script src="{{ _asset('assets/global/plugins/respond.min.js') }}"></script>
+    <script src="{{ _asset('assets/global/plugins/excanvas.min.js') }}"></script> 
+    <script src="{{ _asset('assets/global/plugins/ie8.fix.min.js') }}"></script> 
+    <![endif]-->    
+	@section('head_js_core_plugins')
+    @show
+    
+	@section('head_js_page_level_plugins')
+    @show
+    
+	@section('head_js_theme_global')
+    @show
+    
+    @section('head_js_page_level')
+    @show
+    
+	@section('head_js_theme_layout')
+    @show
+    
+	<!-- BEGIN WECHAT SDK -->
+	<script src="https://res.wx.qq.com/open/js/jweixin-1.1.0.js" type="text/javascript"></script>	
+	<!-- END WECHAT SDK -->	
+	<script src="{{ _asset('assets/js/syrator.js') }}" type="text/javascript" ></script>
+@stop
+
+@section('body')
+
+    @section('content-header')
+    @show
+    
+    @section('content')
+    @show
+        
+    @section('content-footer')
+    @show
+
+@stop
+
+@section('afterBody')
+    
+    @section('extraPlugin')
+    @show
+
+    @section('filledScript')
+    @show
+
+    @section('extraSection')
+    @show
+
+@stop
--- resources/views/_layout/_common.blade.php
+++ resources/views/_layout/_common.blade.php
@@ -1,80 +1,74 @@
-@extends('_layout._base')
+@extends('_layout._base_ex')
 
-@section('head_css')
-    <!-- BEGIN GLOBAL MANDATORY STYLES -->
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-    <link href="{{ _asset('assets/metronic/global/plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ _asset('assets/metronic/global/plugins/simple-line-icons/simple-line-icons.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ _asset('assets/metronic/global/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ _asset('assets/metronic/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css') }}" rel="stylesheet" type="text/css" />
-    <!-- END GLOBAL MANDATORY STYLES -->
-    <!-- BEGIN THEME GLOBAL STYLES -->
-    <link href="{{ _asset('assets/metronic/global/css/components.min.css') }}" rel="stylesheet" id="style_components" type="text/css" />
-    <link href="{{ _asset('assets/metronic/global/css/plugins.min.css') }}" rel="stylesheet" type="text/css" />
-    <!-- END THEME GLOBAL STYLES -->
-    <!-- BEGIN THEME LAYOUT STYLES -->
-    <link href="{{ _asset('assets/metronic/layouts/layout/css/layout.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ _asset('assets/metronic/layouts/layout/css/themes/darkblue.min.css') }}" rel="stylesheet" type="text/css" id="style_color" />
-    <link href="{{ _asset('assets/metronic/layouts/layout/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
-    <!-- END THEME LAYOUT STYLES -->
-	<link href="{{ _asset('assets/css/syrator.css') }}" rel="stylesheet" type="text/css"/>
-@stop
-
-@section('head_js')
+@section('head_css_mandatory')
 @parent
-    <!--[if lt IE 9]>
-    <script src="{{ _asset('assets/global/plugins/respond.min.js') }}"></script>
-    <script src="{{ _asset('assets/global/plugins/excanvas.min.js') }}"></script> 
-    <script src="{{ _asset('assets/global/plugins/ie8.fix.min.js') }}"></script> 
-    <![endif]-->
-    <!-- BEGIN CORE PLUGINS -->
-    <script src="{{ _asset('assets/metronic/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
-    <script src="{{ _asset('assets/metronic/global/plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
-    <script src="{{ _asset('assets/metronic/global/plugins/js.cookie.min.js') }}" type="text/javascript"></script>
-    <script src="{{ _asset('assets/metronic/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
-    <script src="{{ _asset('assets/metronic/global/plugins/jquery.blockui.min.js') }}" type="text/javascript"></script>
-    <script src="{{ _asset('assets/metronic/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}" type="text/javascript"></script>
-    <!-- END CORE PLUGINS -->
-    <!-- BEGIN THEME GLOBAL SCRIPTS -->
-    <script src="{{ _asset('assets/metronic/global/scripts/app.min.js') }}" type="text/javascript"></script>
-    <!-- END THEME GLOBAL SCRIPTS -->
-    <!-- BEGIN THEME LAYOUT SCRIPTS -->
-    <script src="{{ _asset('assets/metronic/layouts/layout/scripts/layout.min.js') }}" type="text/javascript"></script>
-    <script src="{{ _asset('assets/metronic/layouts/layout/scripts/demo.min.js') }}" type="text/javascript"></script>
-    <script src="{{ _asset('assets/metronic/layouts/global/scripts/quick-sidebar.min.js') }}" type="text/javascript"></script>
-    <script src="{{ _asset('assets/metronic/layouts/global/scripts/quick-nav.min.js') }}" type="text/javascript"></script>
-    <!-- END THEME LAYOUT SCRIPTS -->	
-	<!-- BEGIN WECHAT SDK -->
-	<script src="https://res.wx.qq.com/open/js/jweixin-1.1.0.js" type="text/javascript"></script>	
-	<!-- END WECHAT SDK -->	
-	<script src="{{ _asset('assets/js/syrator.js') }}" type="text/javascript" ></script>
+<!-- BEGIN GLOBAL MANDATORY STYLES -->
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+<link href="{{ _asset('assets/metronic/global/plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ _asset('assets/metronic/global/plugins/simple-line-icons/simple-line-icons.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ _asset('assets/metronic/global/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ _asset('assets/metronic/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css') }}" rel="stylesheet" type="text/css" />
+<!-- END GLOBAL MANDATORY STYLES -->
 @stop
 
-@section('body')
+@section('head_css_page_level_plugins')
+@parent
+@show
 
-    @section('content-header')
-    @show
-    
-    @section('content')
-    @show
-        
-    @section('content-footer')
-    @show
+@section('head_css_theme_global')
+@parent
+<!-- BEGIN THEME GLOBAL STYLES -->
+<link href="{{ _asset('assets/metronic/global/css/components.min.css') }}" rel="stylesheet" id="style_components" type="text/css" />
+<link href="{{ _asset('assets/metronic/global/css/plugins.min.css') }}" rel="stylesheet" type="text/css" />
+<!-- END THEME GLOBAL STYLES -->
+@show
 
+@section('head_css_page_level')
+@parent
+@show
+
+@section('head_css_theme_layout')
+@parent
+<!-- BEGIN THEME LAYOUT STYLES -->
+<link href="{{ _asset('assets/metronic/layouts/layout/css/layout.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ _asset('assets/metronic/layouts/layout/css/themes/darkblue.min.css') }}" rel="stylesheet" type="text/css" id="style_color" />
+<link href="{{ _asset('assets/metronic/layouts/layout/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
+<!-- END THEME LAYOUT STYLES -->
+@show
+
+@section('head_js_core_plugins')
+@parent
+<!-- BEGIN CORE PLUGINS -->
+<script src="{{ _asset('assets/metronic/global/plugins/jquery.min.js') }}" type="text/javascript"></script>
+<script src="{{ _asset('assets/metronic/global/plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ _asset('assets/metronic/global/plugins/js.cookie.min.js') }}" type="text/javascript"></script>
+<script src="{{ _asset('assets/metronic/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
+<script src="{{ _asset('assets/metronic/global/plugins/jquery.blockui.min.js') }}" type="text/javascript"></script>
+<script src="{{ _asset('assets/metronic/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js') }}" type="text/javascript"></script>
+<!-- END CORE PLUGINS -->
 @stop
 
-@section('afterBody')
-    
-    @section('extraPlugin')
-    @show
-
-    @section('filledScript')
-    @show
-
-    @section('extraSection')
-    @show
-
+@section('head_js_page_level_plugins')
+@parent
 @stop
 
-@section('front_footer')
+@section('head_js_theme_global')
+@parent
+<!-- BEGIN THEME GLOBAL SCRIPTS -->
+<script src="{{ _asset('assets/metronic/global/scripts/app.min.js') }}" type="text/javascript"></script>
+<!-- END THEME GLOBAL SCRIPTS -->
+@stop
+
+@section('head_js_page_level')
+@parent
+@stop
+
+@section('head_js_theme_layout')
+@parent
+<!-- BEGIN THEME LAYOUT SCRIPTS -->
+<script src="{{ _asset('assets/metronic/layouts/layout/scripts/layout.min.js') }}" type="text/javascript"></script>
+<script src="{{ _asset('assets/metronic/layouts/layout/scripts/demo.min.js') }}" type="text/javascript"></script>
+<script src="{{ _asset('assets/metronic/layouts/global/scripts/quick-sidebar.min.js') }}" type="text/javascript"></script>
+<script src="{{ _asset('assets/metronic/layouts/global/scripts/quick-nav.min.js') }}" type="text/javascript"></script>
+<!-- END THEME LAYOUT SCRIPTS -->
 @stop
