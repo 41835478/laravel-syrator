5ee51d9be06c8542cb158790e224dc0aebb7ca99
syrator <recording@163.com>
2017-07-10 14:17:31 +0800

    syrator <recording@163.com>

--- public/assets/syrator/js/sidebar.js
+++ public/assets/syrator/js/sidebar.js
@@ -2,17 +2,22 @@ $(document).ready(function(){
     var current_url = '//' + window.location.host + window.location.pathname;
     
     var url_path_array = window.location.pathname.split( '/' );
-    if (url_path_array[url_path_array.length-1]==='create') {
+    if (url_path_array[url_path_array.length-2]==='info' && url_path_array[url_path_array.length-1]==='setting') {
+    	current_url = current_url.substring(0,current_url.length-7) + 'overview';
+    } else if (url_path_array[url_path_array.length-2]==='appinfo' && url_path_array[url_path_array.length-1]==='guide') {
+    	current_url = current_url.substring(0,current_url.length-6);
+    } else if (url_path_array[url_path_array.length-3]==='appinfo' && url_path_array[url_path_array.length-2]==='guide') {
+    	current_url = '//' + window.location.host;
+    	for(var i=1; i<url_path_array.length-2;i++){
+    		current_url += '/' + url_path_array[i];
+   		}
+    } else if (url_path_array[url_path_array.length-1]==='create') {
     	current_url = current_url.substring(0,current_url.length-7);
     } else if (url_path_array[url_path_array.length-1]==='edit') {
     	current_url = '//' + window.location.host;
     	for(var i=1; i<url_path_array.length-2;i++){
     		current_url += '/' + url_path_array[i];
    		}
-    } else if (url_path_array[url_path_array.length-2]==='info' && url_path_array[url_path_array.length-1]==='setting') {
-    	current_url = current_url.substring(0,current_url.length-7) + 'overview';
-    } else if (url_path_array[url_path_array.length-2]==='appinfo' && url_path_array[url_path_array.length-1]==='guide') {
-    	current_url = current_url.substring(0,current_url.length-6);
     }
 
     var current_li = $('ul.page-sidebar-menu>li').find('a[href="'+current_url+'"]').closest('li');
