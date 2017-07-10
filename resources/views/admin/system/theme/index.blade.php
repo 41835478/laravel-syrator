901729bda66f307a0492b73bb3d5ae9f239305d6
syrator <recording@163.com>
2017-07-10 10:16:02 +0800

    syrator <recording@163.com>

--- resources/views/admin/system/theme/index.blade.php
+++ resources/views/admin/system/theme/index.blade.php
@@ -142,7 +142,7 @@ jQuery(document).ready(function() {
             title: title,
             shadeClose: false,
             shade: 0,
-            area: ['480px', '215px'],
+            area: ['480px', '480px'],
             content: src
         });
     });
--- resources/views/admin/system/theme/show.blade.php
+++ resources/views/admin/system/theme/show.blade.php
@@ -1,55 +1,72 @@
-@extends('_layout._common')
+@extends('admin._layout._show')
 
-@section('head_css')
+@section('page-content-row')
 @parent
-<link rel="stylesheet" type="text/css" href="{{ _asset('assets/metronic/css/profile.css') }}" />
-@stop
-
-@section('body_attr') class="page-header-fixed" @stop
-
-@section('content')
-<div class="page-container row-fluid" style="margin-top:0px;">
-	<div class="page-content" style="height:220px;min-height:220px;margin-left:0px;">
-		<div class="container-fluid">
-			<div class="row-fluid profile">
-				<div class="span12">
-					<div class="portlet-body form">
-						<table class="table table-hover table-striped table-bordered" style="margin-bottom: 0px;">
-							<tbody>
-    							<tr>
-    								<td style="width: 80px;">模板编码：</td>
-    								<td>{{ $theme->code }}</td>
-    							</tr>
-    							<tr>
-    								<td>模板名称：</td>
-    								<td>{{ $theme->name }}</td>
-    							</tr>
-    							<tr>
-    								<td>模板描述：</td>
-    								<td>{{ $theme->description }}</td>
-    							</tr>
-    							<tr>
-    								<td>作者：</td>
-    								<td>{{ $theme->author }}</td>
-    							</tr>
-    							<tr>
-    								<td>版本：</td>
-    								<td>{{ $theme->version }}</td>
-    							</tr>
-    							<tr>
-    								<td>是否使用：</td>
-    								@if($theme->is_current === '1')
-                					<td class="text-red"><i class="icon-ok"></i></td>
-                					@else
-                					<td class="text-red"><i class="icon-remove"></i></td>
-                					@endif
-    							</tr>
-    						</tbody>
-						</table>					
-					</div>					
-				</div>
-			</div>
-		</div>
-	</div>  
+<div class="row">
+    <div class="col-xs-12 ">
+        <div class="portlet-body form">
+            <form class="form-horizontal" role="form">
+                <div class="form-body" style="padding: 20px 20px 0px 20px !important;">
+                    <div class="form-group">
+                        <label class="col-xs-3 control-label" style="margin-top: 7px;">编码</label>
+                        <div class="col-xs-9">
+                        	<input type="text" class="form-control" placeholder="Readonly" readonly="" value="{{ $theme->code }}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-xs-3 control-label" style="margin-top: 7px;">名称</label>
+                        <div class="col-xs-9">
+                        	<input type="text" class="form-control" placeholder="Readonly" readonly="" value="{{ $theme->name }}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-xs-3 control-label" style="margin-top: 7px;">描述</label>
+                        <div class="col-xs-9">
+                        	<input type="text" class="form-control" placeholder="Readonly" readonly="" value="{{ $theme->description }}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-xs-3 control-label" style="margin-top: 7px;">版本</label>
+                        <div class="col-xs-9">
+                        	<input type="text" class="form-control" placeholder="Readonly" readonly="" value="{{ $theme->version }}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-xs-3 control-label" style="margin-top: 7px;">作者</label>
+                        <div class="col-xs-9">
+                        	<input type="text" class="form-control" placeholder="Readonly" readonly="" value="{{ $theme->author }}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-xs-3 control-label" style="margin-top: 7px;">是否使用</label>
+                        <div class="col-xs-9">
+        					<div class="mt-radio-inline">
+                        		<label class="mt-radio">
+                        			<input disabled="disabled" type="radio" name="is_current" value="0" {{($theme->is_current == '0')?'checked':''}}/>否
+                        			<span></span>
+                        		</label>
+                        		<label class="mt-radio">
+                        			<input disabled="disabled" type="radio" name="is_current" value="1" {{($theme->is_current == '1')?'checked':''}}/>是
+                        			<span></span>
+                        		</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-xs-3 control-label" style="margin-top: 7px;">创建时间</label>
+                        <div class="col-xs-9">
+                        	<input type="text" class="form-control" placeholder="Readonly" readonly="" value="{{ $theme->created_at }}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-xs-3 control-label" style="margin-top: 7px;">更新时间</label>
+                        <div class="col-xs-9">
+                        	<input type="text" class="form-control" placeholder="Readonly" readonly="" value="{{ $theme->updated_at }}">
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 @stop
