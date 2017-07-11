84b1214a7beb49c6107ecc04f31cc1506f801fc4
syrator <recording@163.com>
2017-07-12 02:00:47 +0800

    syrator <recording@163.com>

--- modules/Wechat/Http/routes.php
+++ modules/Wechat/Http/routes.php
@@ -1,15 +1,10 @@
 <?php
 
 Route::group(['middleware' => 'web', 'prefix' => 'wechat', 'namespace' => 'Modules\Wechat\Http\Controllers'], function()
-{
-    Route::group(['prefix' => 'access', 'namespace' => 'Access', 'middleware' => ['web']], function () {
-        Route::any('/', 'AccessController@access');
-    });
-    
+{    
 	Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth:admin']], function () {
 	    // 后台首页：控制台
 	    Route::get('/', 'WechatController@index');
-	    Route::get('index', 'WechatController@index');
 	    Route::get('params', 'WechatController@getParams');
 	    Route::put('params', 'WechatController@putParams');
     });
