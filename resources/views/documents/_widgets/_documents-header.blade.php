0671bd066cc8eac9305fd4d46fdf18216694b889
syrator <recording@163.com>
2017-07-14 10:00:46 +0800

    syrator <recording@163.com>

--- resources/views/documents/_widgets/_documents-header.blade.php
+++ resources/views/documents/_widgets/_documents-header.blade.php
@@ -11,28 +11,159 @@
         <a href="javascript:;" class="menu-toggler responsive-toggler" data-toggle="collapse" data-target=".navbar-collapse">
             <span></span>
         </a>
-		<div class="top-menu">
-			<ul class="nav navbar-nav pull-right">
-				<li class="dropdown dropdown-user">
-					<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+        @if(null !== auth()->guard('member')->user() && !empty(auth()->guard('member')->user()))
+        <div class="top-menu">
+            <ul class="nav navbar-nav pull-right">
+            	<li class="dropdown dropdown-extended dropdown-notification" id="header_notification_bar">
+                    <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                        <i class="icon-bell"></i>
+                        <span class="badge badge-default"> 7 </span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="external">
+                            <h3><span class="bold">12 未读</span> 通知</h3>
+                            <a href="">查看所有</a>
+                        </li>
+                        <li>
+                            <ul class="dropdown-menu-list scroller" style="height: 250px;" data-handle-color="#637283">
+                                <li>
+                                    <a href="javascript:;">
+                                        <span class="time">刚刚</span>
+                                        <span class="details">
+                                            <span class="label label-sm label-icon label-success">
+                                                <i class="fa fa-plus"></i>
+                                            </span>
+                                            <span>新用户注册</span> 
+                                        </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:;">
+                                        <span class="time">3分钟前</span>
+                                        <span class="details">
+                                            <span class="label label-sm label-icon label-danger">
+                                                <i class="fa fa-bolt"></i>
+                                            </span>
+                                            <span>服务重载</span>
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown dropdown-extended dropdown-inbox" id="header_inbox_bar">
+                    <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                        <i class="icon-envelope-open"></i>
+                        <span class="badge badge-default"> 4 </span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="external">
+                            <h3>您有<span class="bold">7条新</span>信息</h3>
+                            <a href="">查看所有</a>
+                        </li>
+                        <li>
+                            <ul class="dropdown-menu-list scroller" style="height: 275px;" data-handle-color="#637283">
+                                <li>
+                                    <a href="#">
+                                        <span class="photo">
+                                            <img src="/assets/metronic/layouts/layout3/img/avatar2.jpg" class="img-circle" alt=""> </span>
+                                        <span class="subject">
+                                            <span class="from"> Lisa Wong </span>
+                                            <span class="time">刚刚 </span>
+                                        </span>
+                                        <span class="message"> Vivamus sed auctor nibh congue nibh. auctor nibh auctor nibh... </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <span class="photo">
+                                            <img src="/assets/metronic/layouts/layout3/img/avatar3.jpg" class="img-circle" alt=""> </span>
+                                        <span class="subject">
+                                            <span class="from"> Richard Doe </span>
+                                            <span class="time">16 分钟前 </span>
+                                        </span>
+                                        <span class="message"> Vivamus sed congue nibh auctor nibh congue nibh. auctor nibh auctor nibh... </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <span class="photo">
+                                            <img src="/assets/metronic/layouts/layout3/img/avatar1.jpg" class="img-circle" alt=""> </span>
+                                        <span class="subject">
+                                            <span class="from"> Bob Nilson </span>
+                                            <span class="time">2 小时前 </span>
+                                        </span>
+                                        <span class="message"> Vivamus sed nibh auctor nibh congue nibh. auctor nibh auctor nibh... </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown dropdown-extended dropdown-tasks" id="header_task_bar">
+                    <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                        <i class="icon-calendar"></i>
+                        <span class="badge badge-default"> 3 </span>
+                    </a>
+                    <ul class="dropdown-menu extended tasks">
+                        <li class="external">
+                            <h3>您有<span class="bold">12项待处理</span>任务</h3>
+                            <a href="">查看所有</a>
+                        </li>
+                        <li>
+                            <ul class="dropdown-menu-list scroller" style="height: 275px;" data-handle-color="#637283">
+                                <li>
+                                    <a href="javascript:;">
+                                        <span class="task">
+                                            <span class="desc">新版本发布 v1.2 </span>
+                                            <span class="percent">30%</span>
+                                        </span>
+                                        <span class="progress">
+                                            <span style="width: 40%;" class="progress-bar progress-bar-success" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">
+                                                <span class="sr-only">40% 已完成</span>
+                                            </span>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:;">
+                                        <span class="task">
+                                            <span class="desc">应用程序部署</span>
+                                            <span class="percent">65%</span>
+                                        </span>
+                                        <span class="progress">
+                                            <span style="width: 65%;" class="progress-bar progress-bar-danger" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+                                                <span class="sr-only">65% 已完成</span>
+                                            </span>
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown dropdown-user">
+                    <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
                         <img alt="" class="img-circle" src="{{ auth()->guard('member')->user()->avatar }}" />
-                        <span class="username username-hide-on-mobile"> {{ auth()->guard('member')->user()->nickname }} </span>
+                        <span class="username username-hide-on-mobile"> {{auth()->guard('member')->user()->nickname }} </span>
                         <i class="fa fa-angle-down"></i>
                     </a>
-					<ul class="dropdown-menu dropdown-menu-default">
-						<li>
-							<a href="{{ site_url('mine/info/overview', 'member') }}"><i class="icon-user"></i> 个人资料</a>
-						</li>
-						<li>
-							<a href="{{ site_url('mine/task', 'member') }}"><i class="icon-rocket"></i> 我的任务</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="{{ site_url('auth/logout', 'member') }}"><i class="icon-key"></i> 退出</a>
-						</li>
-					</ul>
-				</li>
-			</ul>
-		</div>
+                    <ul class="dropdown-menu dropdown-menu-default">
+                        <li>
+                            <a href="{{ site_url('mine/info', 'member') }}"><i class="icon-user"></i>个人资料</a>
+                        </li>
+                        <li>
+                            <a href=""><i class="icon-rocket"></i>我的任务<span class="badge badge-success"> 7 </span></a>
+                        </li>
+                        <li class="divider"> </li>
+                        <li>
+                            <a href="{{ site_url('auth/logout', 'member') }}"><i class="icon-key"></i>退出</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        @endif
 	</div>
 </div>
