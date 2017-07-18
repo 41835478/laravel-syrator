0f7d75224a7eb51d1a18f2db694544c497b433fa
syrator <recording@163.com>
2017-07-18 16:10:52 +0800

    syrator <recording@163.com>

--- resources/views/_widgets/edit/form-group-image.blade.php
+++ resources/views/_widgets/edit/form-group-image.blade.php
@@ -5,10 +5,29 @@
     	<span class="required" aria-required="true"> * </span>
     	@endif
     </label>
-	<div class="col-md-9">
-		<textarea class="form-control" rows="6" type="text" id="{{$field->name}}" name="{{$field->name}}" autocomplete="{{$field->autocomplete}}" placeholder="{{$field->placeholder}}" >{{$field->value}}</textarea>
-		<span class="help-block">{{$field->help}}</span>
-	</div>
+	<div class="col-md-4">
+        <div class="fileinput fileinput-new" data-provides="fileinput">
+            <div class="fileinput-new thumbnail" style="width: 150px; height: 150px;">
+            @if (empty($field->value))
+                <img src="http://www.placehold.it/150x150/EFEFEF/AAAAAA&amp;text=no+image" alt="" />
+            @else
+            	<img src="{{ $field->value }}" alt="" />
+            @endif
+            </div>
+            <div class="fileinput-preview fileinput-exists thumbnail" style="max-width: 150px; max-height: 150px;">
+            </div>
+            <div>
+                <span class="btn default btn-file">
+                    <span class="fileinput-new"> 选择图片 </span>
+                    <span class="fileinput-exists"> 修改 </span>
+                    <input type="file" name="file_picture_{{$field->name}}" id="file_picture_{{$field->name}}" accept=".jpg,.png,.gif,.bmp" >
+                </span>
+                <a href="javascript:;" class="btn default fileinput-exists" data-dismiss="fileinput"> 移除 </a>
+				<a id="submit_upload_picture_{{$field->name}}" class="btn default fileinput-exists">上传</a>
+            </div>
+            <input type="hidden" id="picture_thumb_{{$field->name}}" name="{{$field->name}}" value="{{ $field->value }}">
+        </div>
+    </div>
 </div>
 @elseif($field->show_type=="readonly")
 <div class="form-group">
@@ -17,10 +36,18 @@
     	<span class="required" aria-required="true"> * </span>
     	@endif
     </label>
-	<div class="col-md-9">
-		<textarea readonly="readonly" class="form-control" rows="6" type="text" id="{{$field->name}}" name="{{$field->name}}" autocomplete="{{$field->autocomplete}}" placeholder="{{$field->placeholder}}" >{{$field->value}}</textarea>
-		<span class="help-block">{{$field->help}}</span>
-	</div>
+	<div class="col-md-4">
+        <div class="fileinput fileinput-new" data-provides="fileinput">
+            <div class="fileinput-new thumbnail" style="width: 150px; height: 150px;">
+            @if (empty($field->value))
+                <img src="http://www.placehold.it/150x150/EFEFEF/AAAAAA&amp;text=no+image" alt="" />
+            @else
+            	<img src="{{ $field->value }}" alt="" />
+            @endif
+            </div>
+            <input readonly="readonly" type="hidden" id="picture_thumb_{{$field->name}}" name="{{$field->name}}" value="{{ $field->value }}">
+        </div>
+    </div>
 </div>
 @elseif($field->show_type=="disabled")
 <div class="form-group">
@@ -29,9 +56,17 @@
     	<span class="required" aria-required="true"> * </span>
     	@endif
     </label>
-	<div class="col-md-9">
-		<textarea disabled="disabled" class="form-control" rows="6" type="text" id="{{$field->name}}" name="{{$field->name}}" autocomplete="{{$field->autocomplete}}" placeholder="{{$field->placeholder}}" >{{$field->value}}</textarea>
-		<span class="help-block">{{$field->help}}</span>
-	</div>
+	<div class="col-md-4">
+        <div class="fileinput fileinput-new" data-provides="fileinput">
+            <div class="fileinput-new thumbnail" style="width: 150px; height: 150px;">
+            @if (empty($field->value))
+                <img src="http://www.placehold.it/150x150/EFEFEF/AAAAAA&amp;text=no+image" alt="" />
+            @else
+            	<img src="{{ $field->value }}" alt="" />
+            @endif
+            </div>
+            <input disabled="disabled" type="hidden" id="picture_thumb_{{$field->name}}" name="{{$field->name}}" value="{{ $field->value }}">
+        </div>
+    </div>
 </div>
 @endif
